51c55a62aea4f5742b1f3111fd2040286dd5b8a3
vivian <viviangomesf12@gmail.com>
2024-08-26 14:08:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const botaoNao = document.getElementById('nao');
+
+//     botaoNao.addEventListener('mouseover', () => {
+//         botaoNao.style.display = 'none';
+//     });
+// });
